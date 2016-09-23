30600e645feca8a1fc1538eba187afe03e15da6c
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-09-23 16:52:08 -0700

    Remove Thor

---
+++ lib/test_launcher/cli/input_parser.rb
@@ -0,0 +1,55 @@
+require "optparse"
+# This could use some love
+
+class InputParser
+  BANNER = <<-DESC
+Find tests and run them by trying to match an individual test or the name of a test file(s).
+
+See full README: https://github.com/petekinnecom/test_launcher
+
+Usage: `test_launcher "search string" [--all]`
+
+  DESC
+
+  def initialize(args)
+    @query = args
+    @options = {}
+    option_parser.parse!(args)
+  end
+
+  def query
+    if @query.size == 0
+      puts option_parser
+      exit
+    elsif @query.size > 1
+      puts "Concatenating args to single string. (see https://github.com/petekinnecom/test_launcher)"
+    end
+
+    @query.join(" ")
+  end
+
+  def options
+    @options
+  end
+
+  private
+
+  def option_parser
+    OptionParser.new do |opts|
+      opts.banner = BANNER
+
+      opts.on("-a", "--all", "Run all matching tests. Defaults to false.") do
+        options[:run_all] = true
+      end
+
+      opts.on("-h", "--help", "Prints this help") do
+        puts opts
+        exit
+      end
+
+      opts.on("-f", "--framework framework", "The testing framework being used. Valid options: ['minitest', 'rspec', 'guess']. Defaults to 'guess'") do |framework|
+        options[:framework] = framework
+      end
+    end
+  end
+end
--- test_launcher.gemspec
+++ test_launcher.gemspec
@@ -18,7 +18,6 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_dependency("thor", [">= 0.19", "< 1.0"])
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
 end
