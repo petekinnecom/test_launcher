f27f58c8dc36a63b06590e9c1b639c30a0b4af90
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-09-23 11:17:04 -0700

    Hacked jetbrains

--- lib/test_launcher/jetbrains.rb
+++ lib/test_launcher/jetbrains.rb
@@ -1,12 +1,46 @@
 require "test_launcher/frameworks/minitest"
 
 module TestLauncher
-  module Jetbrains
+  class Jetbrains
 
     def self.launch
-      test_case = Frameworks::Minitest::TestCase.new(file: ARGV.first, example: ARGV.last.split("=").last)
+      new(ARGV).launch
+    end
+
+    def initialize(args)
+      @args = args
+    end
+
+    def launch
       Dir.chdir('/')
-      exec TestLauncher::Frameworks::Minitest::Runner.new.single_example(test_case)
+      puts "Using test_launcher to run:"
+      puts command
+      puts ''
+      `echo ''` # sync to stdout or something, I don't know but this makes it display
+      exec command
+    end
+
+    private
+
+    def command
+      if test_case.is_example?
+        %{cd #{test_case.app_root} && ruby -I test #{test_case.relative_test_path} --name='#{test_case.example}'}
+      else
+        TestLauncher::Frameworks::Minitest::Runner.new.one_or_more_files([test_case])
+      end
+    end
+
+    def test_case
+      @test_case ||=
+        if args[-1].match('--name=')
+          Frameworks::Minitest::TestCase.new(file: args[-2], example: args[-1][/--name=(.*)/, 1])
+        else
+          Frameworks::Minitest::TestCase.new(file: args[-1])
+        end
+    end
+
+    def args
+      @args
     end
   end
 end
