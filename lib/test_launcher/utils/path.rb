6a53850deaa207e545584a46cdce9c7b0f4353c3
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-09-23 12:32:37 -0700

    Use full system path for initial 'cd'

--- lib/test_launcher/utils/path.rb
+++ lib/test_launcher/utils/path.rb
@@ -5,27 +5,7 @@ module TestLauncher
     module Path
 
       def self.split(filename)
-        self.prepend_pwd(Pathname.new(filename).each_filename.to_a)
-      end
-
-      def self.path_for(filename)
-        self.split(File.split(file)[0])
-      end
-
-      def self.join(*array)
-        File.join(*array)
-      end
-
-      def self.prepend_pwd(dirs)
-        if dirs[0] == "."
-          dirs
-        else
-          ["."] + dirs
-        end
-      end
-
-      def self.relative_join(array)
-        join(prepend_pwd(array))
+        Pathname.new(filename).each_filename.to_a
       end
     end
   end
--- test/test_launcher/minitest_integration_test.rb
+++ test/test_launcher/minitest_integration_test.rb
@@ -20,43 +20,49 @@ module TestLauncher
 
     def test__single_method
       TestLauncher.launch("file_name_1__method_name_1", framework: "minitest")
-      assert_equal "cd ./test/test_launcher/fixtures/minitest && ruby -I test test/class_1_test.rb --name=/file_name_1__method_name_1/", Shell::Runner.recall_exec
+      assert_equal "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test test/class_1_test.rb --name=/file_name_1__method_name_1/", Shell::Runner.recall_exec
     end
 
     def test__multiple_methods__same_file
       TestLauncher.launch("file_name_1", framework: "minitest")
-      assert_equal "cd ./test/test_launcher/fixtures/minitest && ruby -I test test/class_1_test.rb --name=/file_name_1/", Shell::Runner.recall_exec
+      assert_equal "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test test/class_1_test.rb --name=/file_name_1/", Shell::Runner.recall_exec
     end
 
     def test__multiple_methods__different_files
       TestLauncher.launch("multiple_files__same_method", framework: "minitest")
-      assert_equal "cd ./test/test_launcher/fixtures/minitest && ruby -I test test/class_2_test.rb --name=/multiple_files__same_method/", Shell::Runner.recall_exec
+      assert_equal "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test test/class_2_test.rb --name=/multiple_files__same_method/", Shell::Runner.recall_exec
     end
 
     def test__single_file
       TestLauncher.launch("class_1_test", framework: "minitest")
-      assert_equal "cd ./test/test_launcher/fixtures/minitest && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb", Shell::Runner.recall_exec
+      assert_equal "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb", Shell::Runner.recall_exec
     end
 
     def test__multiple_files
       TestLauncher.launch("Root1""Dum""myTest""Class", framework: "minitest") # don't trigger the find in *this* file
-      assert_equal "cd ./test/test_launcher/fixtures/minitest && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_2_test.rb", Shell::Runner.recall_exec
+      assert_equal "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_2_test.rb", Shell::Runner.recall_exec
     end
 
     def test__multiple_files__all
       TestLauncher.launch("Root1""DummyTest""Class", run_all: true, framework: "minitest") # don't trigger the find in *this* file
-      assert_equal "cd ./test/test_launcher/fixtures/minitest && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb test/class_2_test.rb", Shell::Runner.recall_exec
+      assert_equal "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb test/class_2_test.rb", Shell::Runner.recall_exec
     end
 
     def test__multiple_files__different_roots__all
       TestLauncher.launch("DummyTest""Class", run_all: true, framework: "minitest") # don't trigger the find in *this* file
-      expected = "cd ./test/test_launcher/fixtures/minitest && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb test/class_2_test.rb; cd -;\n\ncd ./test/test_launcher/fixtures/minitest/test/different_root && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/different_root_test.rb"
+      expected = "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb test/class_2_test.rb; cd -;\n\ncd #{system_path("test/test_launcher/fixtures/minites")}t/test/different_root && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/different_root_test.rb"
       assert_equal expected, Shell::Runner.recall_exec
     end
 
     def test__regex
       TestLauncher.launch("Root1""DummyTest""Class1""Test", framework: "minitest") # don't trigger the find in *this* file
-      assert_equal "cd ./test/test_launcher/fixtures/minitest && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb", Shell::Runner.recall_exec
+      assert_equal "cd #{system_path("test/test_launcher/fixtures/minitest")} && ruby -I test -e 'ARGV.each { |file| require(Dir.pwd + \"/\" + file) }' test/class_1_test.rb", Shell::Runner.recall_exec
+    end
+
+    private
+
+    def system_path(relative_dir)
+      File.join(Dir.pwd, relative_dir)
     end
   end
 end
