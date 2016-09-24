448f131d189087a528765c44fdf855cf760dca52
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-09-24 13:00:10 -0700

    move modules, remove modules, re-move modules

--- lib/test_launcher.rb
+++ lib/test_launcher.rb
@@ -4,18 +4,19 @@ require "test_launcher/shell/runner"
 require "test_launcher/search/git"
 require "test_launcher/frameworks"
 
-
 module TestLauncher
   def self.launch(input, framework: "guess", run_all: false)
     shell = Shell::Runner.new(log_path: '/tmp/test_launcher.log')
-
     searcher = Searchers::Git.new(shell)
-    framework = Frameworks.guess_framework(framework)
 
-    search_results = framework::SearchResults.new(input, searcher).prioritized_results
-    runner = framework::Runner.new
+    command = Frameworks.locate(
+      framework_name: framework,
+      shell: shell,
+      searcher: searcher,
+      input: input,
+      run_all: run_all
+    )
 
-    command = Frameworks::Implementation::Consolidator.consolidate(search_results, shell, runner, run_all)
     shell.exec command
   end
 end
--- lib/test_launcher/frameworks.rb
+++ lib/test_launcher/frameworks.rb
@@ -2,9 +2,16 @@ require "test_launcher/frameworks/base"
 require "test_launcher/frameworks/minitest"
 require "test_launcher/frameworks/rspec"
 
-
 module TestLauncher
   module Frameworks
+    def self.locate(framework_name:, input:, run_all:, shell:, searcher:)
+      framework = guess_framework(framework_name)
+      search_results = framework::Locator.new(input, searcher).prioritized_results
+      runner = framework::Runner.new
+
+      Implementation::Consolidator.consolidate(search_results, shell, runner, run_all)
+    end
+
     def self.guess_framework(framework_name)
       if framework_name == "rspec"
         RSpec
