538926de7d27b0a435e645921ab2709ed91be2ac
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-09-25 10:18:38 -0700

    Namespace InputParser

--- lib/test_launcher/cli/input_parser.rb
+++ lib/test_launcher/cli/input_parser.rb
@@ -1,11 +1,12 @@
 require "test_launcher/version"
 require "optparse"
-# This could use some love
 
-class InputParser
-  ParseError = Class.new(RuntimeError)
+module TestLauncher
+  module CLI
+    class InputParser
+      ParseError = Class.new(RuntimeError)
 
-  BANNER = <<-DESC
+      BANNER = <<-DESC
 Find tests and run them by trying to match an individual test or the name of a test file(s).
 
 See full README: https://github.com/petekinnecom/test_launcher
@@ -14,56 +15,58 @@ Usage: `test_launcher "search string" [--all]`
 
 VERSION: #{TestLauncher::VERSION}
 
-  DESC
+      DESC
 
-  def initialize(args)
-    @query = args
-    @options = {}
-    option_parser.parse!(args)
-  rescue OptionParser::ParseError
-    puts "Invalid arguments"
-    puts "----"
-    puts option_parser
-    exit
-  end
-
-  def query
-    if @query.size == 0
-      puts option_parser
-      exit
-    elsif @query.size > 1
-      puts "Concatenating args to single string. (see https://github.com/petekinnecom/test_launcher)"
-    end
-
-    @query.join(" ")
-  end
-
-  def options
-    @options
-  end
-
-  private
-
-  def option_parser
-    OptionParser.new do |opts|
-      opts.banner = BANNER
-
-      opts.on("-a", "--all", "Run all matching tests. Defaults to false.") do
-        options[:run_all] = true
-      end
-
-      opts.on("-h", "--help", "Prints this help") do
-        puts opts
+      def initialize(args)
+        @query = args
+        @options = {}
+        option_parser.parse!(args)
+      rescue OptionParser::ParseError
+        puts "Invalid arguments"
+        puts "----"
+        puts option_parser
         exit
       end
 
-      opts.on("-v", "--version", "Display the version info") do
-        puts TestLauncher::VERSION
-        exit
+      def query
+        if @query.size == 0
+          puts option_parser
+          exit
+        elsif @query.size > 1
+          puts "Concatenating args to single string. (see https://github.com/petekinnecom/test_launcher)"
+        end
+
+        @query.join(" ")
       end
 
-      opts.on("-f", "--framework framework", "The testing framework being used. Valid options: ['minitest', 'rspec', 'guess']. Defaults to 'guess'") do |framework|
-        options[:framework] = framework
+      def options
+        @options
+      end
+
+      private
+
+      def option_parser
+        OptionParser.new do |opts|
+          opts.banner = BANNER
+
+          opts.on("-a", "--all", "Run all matching tests. Defaults to false.") do
+            options[:run_all] = true
+          end
+
+          opts.on("-h", "--help", "Prints this help") do
+            puts opts
+            exit
+          end
+
+          opts.on("-v", "--version", "Display the version info") do
+            puts TestLauncher::VERSION
+            exit
+          end
+
+          opts.on("-f", "--framework framework", "The testing framework being used. Valid options: ['minitest', 'rspec', 'guess']. Defaults to 'guess'") do |framework|
+            options[:framework] = framework
+          end
+        end
       end
     end
   end
