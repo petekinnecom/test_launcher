17421113688f359d0f7b8b0429f97f2aef78b2a7
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-12-18 22:19:36 -0800

    Implement new Search mocking. Failing test due to mtime issue

---
+++ test/test_helpers/mocks/searcher_mock.rb
@@ -0,0 +1,75 @@
+require "test_helpers/mock"
+require "test_launcher/search/git"
+
+module TestLauncher
+  class MemorySearcher < Mock
+    class FileBuilder
+      def path(path)
+        @path = path
+      end
+
+      def contents(string)
+        @lines = string.split("\n")
+      end
+
+      def mtime(time) # TODO: this doesn't work yet!
+        @mtime = time
+      end
+
+      def to_file_mock
+        FileMock.new(@path, @lines, @time)
+      end
+    end
+
+    class FileMock < Struct.new(:path, :lines, :mtime)
+      def grep(regex)
+        lines
+          .each_with_index
+          .map {|line_text, line_number|
+            {
+              file: path,
+              line: line_text.strip,
+              line_number: line_number + 1
+            }
+          }
+          .select {|result|
+            result[:line].match(regex)
+          }
+      end
+    end
+
+    mocks Search::Git
+
+    def initialize
+      yield(self)
+    end
+
+    def find_files(glob_pattern)
+      file_mocks_for_pattern(glob_pattern).map(&:path)
+    end
+
+    def grep(regex, file_pattern: '*')
+      file_mocks_for_pattern(file_pattern)
+        .flat_map { |file|
+          file.grep(regex)
+        }
+    end
+
+    def mock_file
+      file_builder = FileBuilder.new
+      yield(file_builder)
+      file_mocks << file_builder.to_file_mock
+    end
+
+    private
+
+    def file_mocks_for_pattern(glob_pattern)
+      regex = glob_pattern.gsub("*", ".*")
+      file_mocks.select {|fm| fm.path.match(regex)}
+    end
+
+    def file_mocks
+      @file_mocks ||= []
+    end
+  end
+end
--- test/test_launcher/queries/full_regex_query_test.rb
+++ test/test_launcher/queries/full_regex_query_test.rb
@@ -1,71 +1,89 @@
 require "test_helper"
 require "test_helpers/mocks"
 require "test_launcher/queries"
+require "test_launcher/frameworks/minitest"
 
 module TestLauncher
   module Queries
     class FullRegexQueryTest < TestCase
       include DefaultMocks
 
+      def raw_searcher
+        @raw_searcher ||= MemorySearcher.new do |searcher|
+          searcher.mock_file do |f|
+            f.path "single_test.rb"
+            f.contents <<-RB
+              class SingleTest
+                def test__1
+                  matches_single
+                end
+              end
+            RB
+          end
+
+          searcher.mock_file do |f|
+            f.path "multiple_matches_1_test.rb"
+            f.contents <<-RB
+              class MultipleMatches1Test
+                def test__1
+                  multiple_matches_1
+                end
+
+                def test__2
+                  multiple_matches_1
+                end
+              end
+            RB
+          end
+
+          searcher.mock_file do |f|
+            f.path "multiple_matches_2_test.rb"
+            f.contents <<-RB
+              class MultipleMatches2Test
+                def test__1
+                  multiple_matches_2
+                end
+
+                def test__2
+                  multiple_matches_2
+                end
+              end
+            RB
+          end
+        end
+      end
+
       def searcher
-        @searcher ||= MockSearcher.new do |m|
-          m.impl :grep do |regex|
-            case [regex]
-            when ["not_found"]
-              []
-            when ["single"]
-              [{file: "single_test.rb", line: "matches single", line_number: 17}]
-            when ["multiple_same_file"]
-              [
-                {file: "multiple_test.rb", line: "matches multiple_same_file", line_number: 17},
-                {file: "multiple_test.rb", line: "matches multiple_same_file", line_number: 29},
-                {file: "multiple_test.rb", line: "matches multiple_same_file", line_number: 31},
-              ]
-            when ["multiple_different_files"]
-              [
-                {file: "multiple_1_test.rb", line: "matches multiple_different_files", line_number: 17},
-                {file: "multiple_2_test.rb", line: "matches multiple_different_files", line_number: 29},
-              ]
+        @searcher ||= Frameworks::Minitest::Searcher.new(raw_searcher)
+      end
+
+      def runner
+        @runner ||= MockRunner.new do |m|
+          m.impl :single_file do |test_case|
+            case test_case.file
+            when "single_test.rb"
+              "single_file single_test.rb"
+            when "multiple_matches_1_test.rb"
+              "single_file multiple_matches_1_test.rb"
             else
-              raise "unmocked search_string: #{file}, #{line_number}"
+              raise "unmocked single_file: #{test_case.file}"
+            end
+          end
+
+          m.impl :multiple_files do |test_cases|
+            case test_cases.map(&:file)
+            when ["multiple_matches_1_test.rb", "multiple_matches_2_test.rb"]
+              "multiple_files multiple_matches_1_test.rb multiple_matches_2_test.rb"
+            else
+              raise "unmocked multiple_files: #{test_cases}"
             end
           end
         end
+
       end
 
       def create_mock_request(**attrs)
-        MockRequest.new(**attrs) do |m|
-          m.impl(:test_case) do |file:, example: nil, request:|
-            case [file, example]
-            when ["single_test.rb", nil]
-              single_file_test_case
-            when ["multiple_test.rb", nil]
-              multiple_test_case
-            when ["multiple_1_test.rb", nil]
-              multiple_1_test_case
-            when ["multiple_2_test.rb", nil]
-              multiple_2_test_case
-            else
-              raise "unmocked file: #{file}"
-            end
-          end
-        end
-      end
-
-      def single_file_test_case
-        @single_file_test_case ||= MockTestCase.new(file: "single_test.rb")
-      end
-
-      def multiple_test_case
-        @multiple_test_case ||= MockTestCase.new(file: "multiple_test.rb")
-      end
-
-      def multiple_1_test_case
-        @multiple_1_test_case ||= MockTestCase.new(file: "multiple_1_test.rb", mtime: Time.now - 3)
-      end
-
-      def multiple_2_test_case
-        @multiple_2_test_case ||= MockTestCase.new(file: "multiple_2_test.rb", mtime: Time.now - 2)
+        MockRequest.new(**attrs)
       end
 
       def test_command__regex_not_found
@@ -82,35 +100,31 @@ module TestLauncher
         request = create_mock_request(
           search_string: "single",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell
         )
 
         command = FullRegexQuery.new(request, default_command_finder).command
 
-        assert_equal [[single_file_test_case]], default_runner.recall(:single_file)
-
-        assert_equal "single_file_return", command
+        assert_equal "single_file single_test.rb", command
       end
 
       def test_command__multiple_matches_same_file
         request = create_mock_request(
-          search_string: "multiple_same_file",
+          search_string: "multiple_matches_1",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell
         )
 
         command = FullRegexQuery.new(request, default_command_finder).command
 
-        assert_equal [[multiple_test_case]], default_runner.recall(:single_file)
-
-        assert_equal "single_file_return", command
+        assert_equal "single_file multiple_matches_1_test.rb", command
       end
 
       def test_command__multiple_matches_different_files__no_all
         request = create_mock_request(
-          search_string: "multiple_different_files",
+          search_string: "multiple_matches",
           searcher: searcher,
           runner: default_runner,
           shell: default_shell,
@@ -119,25 +133,21 @@ module TestLauncher
 
         command = FullRegexQuery.new(request, default_command_finder).command
 
-        assert_equal [[multiple_2_test_case]], default_runner.recall(:single_file)
-
-        assert_equal "single_file_return", command
+        assert_equal "single_file multiple_matches_1_test.rb", command
       end
 
       def test_command__multiple_matches_different_files__all
         request = create_mock_request(
-          search_string: "multiple_different_files",
+          search_string: "multiple_matches",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell,
           run_all?: true
         )
 
         command = FullRegexQuery.new(request, default_command_finder).command
 
-        assert_equal [[[multiple_1_test_case, multiple_2_test_case]]], default_runner.recall(:multiple_files)
-
-        assert_equal "multiple_files_return", command
+        assert_equal "multiple_files multiple_matches_1_test.rb multiple_matches_2_test.rb", command
       end
     end
   end
