7ab593caeda92f1c663b94ca9b01879ac44404f5
Pete Kinnecom <pete.kinnecom@gmail.com>
2016-12-24 13:22:30 -0800

    Convert some Query tests to use MemorySearcher

--- test/test_launcher/queries/example_name_query_test.rb
+++ test/test_launcher/queries/example_name_query_test.rb
@@ -1,74 +1,84 @@
 require "test_helper"
 require "test_helpers/mocks"
 require "test_launcher/queries"
+require "test_launcher/frameworks/minitest"
 
 module TestLauncher
   module Queries
     class ExampleNameQueryTest < TestCase
       include DefaultMocks
 
+      def raw_searcher
+        @raw_searcher ||= MemorySearcher.new do |searcher|
+          searcher.mock_file do |f|
+            f.path "file_1_test.rb"
+            f.mtime Time.now - 3000
+            f.contents <<-RB
+              class SingleTest
+                def test__one_example
+                  matches_single
+                end
+
+                def test__same_file_1
+                end
+
+                def test__same_file_2
+                end
+
+                def test__different_files
+                end
+              end
+            RB
+          end
+
+          searcher.mock_file do |f|
+            f.path "file_2_test.rb"
+            f.mtime Time.now
+            f.contents <<-RB
+              class MultipleMatches1Test
+                def test__different_files
+                end
+              end
+            RB
+          end
+
+          searcher.mock_file do |f|
+            f.path "multiple_matches_2_test.rb"
+            f.mtime Time.now
+            f.contents <<-RB
+              class MultipleMatches2Test
+                def test__1
+                  multiple_matches_2
+                end
+
+                def test__2
+                  multiple_matches_2
+                end
+              end
+            RB
+          end
+        end
+      end
+
       def searcher
-        @searcher ||= MockSearcher.new do |m|
-          m.impl :examples do |search_string|
-            case search_string
-            when "not_found"
-              []
-            when "one_example"
-              [file: "file_1", line: "unused"]
-            when "multiple_examples__one_file"
-              [
-                {file: "file_2", line: "unused"},
-                {file: "file_2", line: "unused"},
-              ]
-            when "multiple_examples__multiple_files"
-              [
-                {file: "file_3", line: "unused"},
-                {file: "file_4", line: "unused"},
-              ]
-            else
-              raise "unmocked search_string: #{search_string}"
-            end
+        @searcher ||= Frameworks::Minitest::Searcher.new(raw_searcher)
+      end
+
+      def runner
+        @runner ||= MockRunner.new do |m|
+
+          m.impl :single_example do |test_case|
+            "single_example #{test_case.file} #{test_case.example}"
+          end
+
+          m.impl :multiple_files do |test_cases|
+            "multiple_files #{test_cases.map(&:file).join(" ")}"
           end
         end
-      end
-
-      def create_mock_request(**attrs)
-        MockRequest.new(**attrs) do |m|
-          m.impl(:test_case) do |file:, example:, request:|
-            case file
-            when "file_1"
-              file_1_test_case
-            when "file_2"
-              file_2_test_case
-            when "file_3"
-              file_3_test_case
-            when "file_4"
-              file_4_test_case
-            else
-              raise "unmocked file: #{file}"
-            end
-          end
-        end
-      end
-
-      def file_1_test_case
-        @file_1_test_case ||= MockTestCase.new(file: "file_1", mtime: Time.now - 1)
-      end
-
-      def file_2_test_case
-        @file_2_test_case ||= MockTestCase.new(file: "file_2", mtime: Time.now - 2)
-      end
-
-      def file_3_test_case
-        @file_3_test_case ||= MockTestCase.new(file: "file_3", mtime: Time.now - 3)
-      end
-
-      def file_4_test_case
-        @file_4_test_case ||= MockTestCase.new(file: "file_4", mtime: Time.now - 4)
       end
 
       def test_command__example_not_found__returns_nil
-        request = create_mock_request(
+        request = MockRequest.new(
           search_string: "not_found",
           searcher: searcher
         )
@@ -79,25 +89,23 @@ module TestLauncher
       end
 
       def test_command__one_example_found
-        request = create_mock_request(
+        request = MockRequest.new(
           search_string: "one_example",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell
         )
 
         command = ExampleNameQuery.new(request, default_command_finder).command
 
-        assert_equal [[file_1_test_case]], default_runner.recall(:single_example)
-
-        assert_equal "single_example_return", command
+        assert_equal "single_example file_1_test.rb one_example", command
       end
 
       def test_command__one_example_found__notifies
-        request = create_mock_request(
+        request = MockRequest.new(
           search_string: "one_example",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell
         )
 
@@ -110,25 +118,23 @@ module TestLauncher
       end
 
       def test_command__multiple_examples__one_file
-        request = create_mock_request(
-          search_string: "multiple_examples__one_file",
+        request = MockRequest.new(
+          search_string: "same_file",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell
         )
 
         command = ExampleNameQuery.new(request, default_command_finder).command
 
-        assert_equal [[file_2_test_case]], default_runner.recall(:single_example)
-
-        assert_equal "single_example_return", command
+        assert_equal "single_example file_1_test.rb same_file", command
       end
 
       def test_command__multiple_examples__one_file__notifies
-        request = create_mock_request(
-          search_string: "multiple_examples__one_file",
+        request = MockRequest.new(
+          search_string: "same_file",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell
         )
 
@@ -141,26 +147,24 @@ module TestLauncher
       end
 
       def test_command__multiple_examples__multiple_files__no_all
-        request = create_mock_request(
-          search_string: "multiple_examples__multiple_files",
+        request = MockRequest.new(
+          search_string: "different_files",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell,
           run_all?: false
         )
 
         command = ExampleNameQuery.new(request, default_command_finder).command
 
-        assert_equal [[file_3_test_case]], default_runner.recall(:single_example)
-
-        assert_equal "single_example_return", command
+        assert_equal "single_example file_2_test.rb different_files", command
       end
 
       def test_command__multiple_examples__multiple_files__no_all__notifies
-        request = create_mock_request(
-          search_string: "multiple_examples__multiple_files",
+        request = MockRequest.new(
+          search_string: "different_files",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell,
           run_all?: false
         )
@@ -175,26 +179,24 @@ module TestLauncher
       end
 
       def test_command__multiple_examples__multiple_files__all
-        request = create_mock_request(
-          search_string: "multiple_examples__multiple_files",
+        request = MockRequest.new(
+          search_string: "different_files",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell,
           run_all?: true
         )
 
         command = ExampleNameQuery.new(request, default_command_finder).command
 
-        assert_equal [[[file_3_test_case, file_4_test_case]]], default_runner.recall(:multiple_files)
-
-        assert_equal "multiple_files_return", command
+        assert_equal "multiple_files file_1_test.rb file_2_test.rb", command
       end
 
       def test_command__multiple_examples__multiple_files__all__notifies
-        request = create_mock_request(
-          search_string: "multiple_examples__multiple_files",
+        request = MockRequest.new(
+          search_string: "different_files",
           searcher: searcher,
-          runner: default_runner,
+          runner: runner,
           shell: default_shell,
           run_all?: true
         )
@@ -206,7 +208,6 @@ module TestLauncher
         ]
         assert_equal messages, default_shell.recall(:notify)
       end
-
     end
   end
 end
