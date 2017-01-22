45d5167672ad82e265c877e2a7b55dd011c59f67
Pete Kinnecom <pete.kinnecom@gmail.com>
2017-01-21 19:34:59 -0800

    Fix Rubymine request

--- lib/test_launcher/rubymine/request.rb
+++ lib/test_launcher/rubymine/request.rb
@@ -1,14 +1,12 @@
 module TestLauncher
   module Rubymine
     class Request
-      class MinimalRequest
-        def initialize(disable_spring:)
-          @disable_spring = disable_spring
-        end
+      def initialize(disable_spring:)
+        @disable_spring = disable_spring
+      end
 
-        def disable_spring?
-          @disable_spring
-        end
+      def disable_spring?
+        @disable_spring
       end
     end
   end
