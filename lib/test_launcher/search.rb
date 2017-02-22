0f17abf7feb8730500d5c4b13a21ac66650bf04a
Pete Kinnecom <pete.kinnecom@gmail.com>
2017-02-22 09:15:32 -0800

    Revert to using Git searcher since it's faster

--- lib/test_launcher/search.rb
+++ lib/test_launcher/search.rb
@@ -4,15 +4,15 @@ require 'test_launcher/search/git'
 module TestLauncher
   module Search
     def self.searcher(shell)
-      `which ag`
-      implementation =
-        if $?.success?
-          Search::Ag
-        else
-          Search::Git
-        end
+      # `which ag`
+      # implementation =
+      #   if $?.success?
+      #     Search::Ag
+      #   else
+      #     Search::Git
+      #   end
 
-      implementation.new(shell)
+      Search::Git.new(shell)
     end
   end
 end
